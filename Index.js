7f57f20720801b1cfdac871c61360ce94db09356
mac <estheramukwolu@gmail.com>
2026-03-28 21:01:24 +0100

    first commit

--- Index.js
+++ Index.js
@@ -1,22 +1,29 @@
 // generate secret number
 let randomNumber = Math.floor(Math.random()* 50) + 1;
 let attempts = 0;
+const maxAttempts = 5;
 
 // Getting Element
 const guessInput = document.getElementById("guessInput");
 const guessBtn = document.getElementById("guessBtn");
 const message = document.getElementById("message");
 const restartBtn = document.getElementById("restartBtn");
-const attemptsText = document.getElementById("attempts");
+const attemptsText = document.querySelector("#attempts span");
+
+
+function restartGame(){
+    randomNumber = Math.floor(Math.random()* 50) + 1;
+    attempts= 0;
+    attemptsText.textContent = attempts;
+    message.textContent = "";
+    guessInput,value = "";
+    guessBtn.disabled = false
+}
 
 // handling guess
 guessBtn.addEventListener("click",() => {
     const userGuess= Number(guessInput.value);
-
-restartBtn.addEventListener("click",() => {
     
-}
-)    
     if(!userGuess || userGuess < 1 || userGuess > 50){
         message.textContent = "Enter a Number between 1 and 50";
         return;
@@ -24,12 +31,7 @@ restartBtn.addEventListener("click",() => {
 attempts++;
 attemptsText.textContent = `Attempt: ${attempts}`;
 
-if (attempts > 4) {
-    message.textContent ="game over"
-    guessBtndisabled ="true"
-    alert ("Game over, Do you wish to continue")
-}else if (restartBtn){
-}
+
 if(userGuess < randomNumber) {
     message.textContent = "too low";
 
@@ -40,8 +42,16 @@ else if (userGuess > randomNumber){
     message.textContent = `correct! you guessed it in ${attempts} tries`
     guessBtn.disabled = true;
    }
+
+   if(attempts === maxAttempts){
+    message.textContent = `Game over, the number  was ${randomNumber}`;
+    guessBtn.disabled = true;
+
+    setTimeout(restartGame, 2500);
+   } 
     guessInput.value = "";
   });
+  restartBtn.addEventListener("click", restartGame)
 
 
   
